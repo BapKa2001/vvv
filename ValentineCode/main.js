5903fa84307f883bf59059e7679639a5bb8ca970
Nguyen Huu Thien <thienhuunguyen42@gmail.com>
2024-04-27 23:08:15 +0700

    huoi

---
+++ ValentineCode/main.js
@@ -0,0 +1,44 @@
+// Thay đổi nội dung búc thư ở đây
+var letterContent = "Anh biết là hiện tại em đang rất buồn về chuyện tình cảm của em.Nhưng anh mong là em đừng buồn nữa nếu không ai thương em thì em hãy nhớ còn có anh .Anh vẫn luôn thương em như lúc đầu mình quen . ANH IU EM 🙂"
+
+// Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
+durationWrite = 50 
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
